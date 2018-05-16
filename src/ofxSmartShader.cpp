6c0c5b7851266ae3b84b7378f1142e57b63e8865
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2018-05-16 16:03:49 -0700

    improvements

--- src/ofxSmartShader.cpp
+++ src/ofxSmartShader.cpp
@@ -1,70 +1,207 @@
 
 #include "ofxSmartShader.h"
 
+// Research https://github.com/cinder/Cinder/blob/master/src/cinder/gl/ShaderPreprocessor.cpp
+
 ofxSmartShader::ofxSmartShader() {
-	bWatchingFiles = false;
+	m_bWatchingFiles = false;
+    m_bAutoVersionConversion = true;
 }
 
 ofxSmartShader::~ofxSmartShader() {
 	disableWatchFiles();
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
+void ofxSmartShader::addIncludeFolder(std::string &_folder) {
+    m_includeFolders.push_back(_folder);
+}
+
+void ofxSmartShader::addDefineKeyword(std::string &_define) {
+    m_defines.push_back(_define);
+}
+
+void ofxSmartShader::delDefineKeyword(std::string &_define) {
+    for (int i = m_defines.size() - 1; i >= 0 ; i++) {
+        if ( m_defines[i] == _define ) {
+            m_defines.erase(m_defines.begin() + i);
+        }
+    }
+}
+
+std::string getAbsPath(const std::string& _str) {
+    std::string abs_path = realpath(_str.c_str(), NULL);
+    std::size_t found = abs_path.find_last_of("\\/");
+    if (found){
+        return abs_path.substr(0, found);
+    }
+    else {
+        return "";
+    }
+}
+
+std::string urlResolve(const std::string& _path, const std::string& _pwd, const std::vector<std::string> _includeFolders) {
+    std::string url = _pwd + '/' + _path;
+    if (ofFile(url).exists()) {
+        return url;
+    }
+    else {
+        for (uint i = 0; i < _includeFolders.size(); i++) {
+            std::string new_path = _includeFolders[i] + "/" + _path;
+            if (ofFile(new_path).exists()) {
+                return new_path;
+            }
+        }
+        return _path;
+    }
+}
+
+bool loadFromPath(const std::string& _path, std::string* _into, const std::vector<std::string> _includeFolders) {
+    std::ifstream file;
+    std::string buffer;
+    
+    file.open(_path.c_str());
+    if(!file.is_open()) return false;
+    std::string original_path = getAbsPath(_path);
+    
+    while(!file.eof()) {
+        getline(file, buffer);
+        if (buffer.find("#include ") == 0 || buffer.find("#pragma include ") == 0){
+            unsigned begin = buffer.find_first_of("\"");
+            unsigned end = buffer.find_last_of("\"");
+            if (begin != end) {
+                std::string file_name = buffer.substr(begin+1,end-begin-1);
+                file_name = urlResolve(file_name, original_path, _includeFolders);
+                std::string newBuffer;
+                if(loadFromPath(file_name, &newBuffer, _includeFolders)){
+                    (*_into) += "\n" + newBuffer + "\n";
+                }
+                else {
+                    std::cout << file_name << " not found at " << original_path << std::endl;
+                }
+            }
+        } else {
+            (*_into) += buffer + "\n";
+        }
+    }
+    
+    file.close();
+    return true;
+}
+
+bool haveExt(const std::string& file, const std::string& ext){
+    return file.find("."+ext) != std::string::npos;
+}
+
 bool ofxSmartShader::load(string _shaderName ) {
 	return load( _shaderName + ".vert", _shaderName + ".frag", _shaderName + ".geom" );
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 bool ofxSmartShader::load(string _vertName, string _fragName, string _geomName) {
 	unload();
 	
-    ofShader::setGeometryOutputCount(geometryOutputCount);
-    ofShader::setGeometryInputType(geometryInputType);
-    ofShader::setGeometryOutputType(geometryOutputType);
+    ofShader::setGeometryOutputCount( m_geometryOutputCount );
+    ofShader::setGeometryInputType( m_geometryInputType );
+    ofShader::setGeometryOutputType( m_geometryOutputType );
 
 	// hackety hack, clear errors or shader will fail to compile
 	GLuint err = glGetError();
 	
-	lastTimeCheckMillis = ofGetElapsedTimeMillis();
+	m_lastTimeCheckMillis = ofGetElapsedTimeMillis();
 	setMillisBetweenFileCheck( 2 * 1000 );
 	enableWatchFiles();
 	
-	loadShaderNextFrame = false;
+	m_loadShaderNextFrame = false;
 	
-	vertexShaderFilename = _vertName;
-	fragmentShaderFilename = _fragName;
-	geometryShaderFilename = _geomName;
+    // Update filenames
+	m_vertexShaderFilename = _vertName;
+	m_fragmentShaderFilename = _fragName;
+	m_geometryShaderFilename = _geomName;
 	
-	vertexShaderFile.clear();
-	fragmentShaderFile.clear();
-	geometryShaderFile.clear();
+    // Update last change time
+	m_vertexShaderFile.clear();
+	m_fragmentShaderFile.clear();
+	m_geometryShaderFile.clear();
 	
-	vertexShaderFile   = ofFile( ofToDataPath( vertexShaderFilename ) );
-	fragmentShaderFile = ofFile( ofToDataPath( fragmentShaderFilename ) );
-	geometryShaderFile = ofFile( ofToDataPath( geometryShaderFilename ) );
+	m_vertexShaderFile   = ofFile( ofToDataPath( m_vertexShaderFilename ) );
+	m_fragmentShaderFile = ofFile( ofToDataPath( m_fragmentShaderFilename ) );
+	m_geometryShaderFile = ofFile( ofToDataPath( m_geometryShaderFilename ) );
+    
+    m_fileChangedTimes.clear();
+    m_fileChangedTimes.push_back( getLastModified( m_vertexShaderFile ) );
+    m_fileChangedTimes.push_back( getLastModified( m_fragmentShaderFile ) );
+    m_fileChangedTimes.push_back( getLastModified( m_geometryShaderFile ) );
+    
+    // Update Sources
+    std::string vertexSrc = "";
+    std::string fragmentSrc = "";
+    std::string geometrySrc = "";
+    
+    // 1. Load shaders resolving #include to nested sources
+    loadFromPath( ofToDataPath( m_vertexShaderFilename ), &vertexSrc, m_includeFolders );
+    loadFromPath( ofToDataPath( m_fragmentShaderFilename ), &fragmentSrc, m_includeFolders );
+    #ifndef TARGET_OPENGLES
+    loadFromPath( ofToDataPath( m_geometryShaderFilename ), &geometrySrc, m_includeFolders );
+    #endif
+    
+    // 2. Add defines
+    std::string defines_header = "";
+    for (unsigned int i = 0; i < m_defines.size(); i++) {
+        defines_header += "#define " + m_defines[i] + "\n";
+    }
+    defines_header += "#line 0 \n";
+    
+    // 2. Check active default uniforms
+    m_time = false;
+    m_date = false;
+    m_delta = false;
+    m_mouse = false;
+    m_resolution = false;
+    _checkActiveUniforms(vertexSrc);
+    _checkActiveUniforms(fragmentSrc);
+    _checkActiveUniforms(geometrySrc);
+    
+    // 3. Add defines
+    string version_vert_header = "";
+    string version_frag_header = "";
+    string version_geom_header = "";
+    
+    if (m_bAutoVersionConversion) {
+#ifdef TARGET_OPENGLES
+        string version100 = "#ifdef GL_ES\n\
+precision highp float;\n\
+#endif\n";
+        version_vert_header = version100;
+        version_frag_header = version100;
+#else
+        if (ofIsGLProgrammableRenderer()) {
+            version_vert_header = "#version 150\n\
+#define attribute in\n\
+#define varying out\n";
+            version_frag_header = "#version 150\n\
+#define varying in\n\
+#define gl_FragColor fragColor\n\
+out vec4 fragColor;\n";
+            version_geom_header = "#version 150\n";
+        }
+        else {
+            version_vert_header = "#version 120\n";
+            version_frag_header = "#version 120\n";
+            version_geom_header = "#version 120\n";
+        }
+#endif
+    }
 	
-	ofBuffer vertexShaderBuffer = ofBufferFromFile( ofToDataPath( vertexShaderFilename ) );
-	ofBuffer fragmentShaderBuffer = ofBufferFromFile( ofToDataPath( fragmentShaderFilename ) );
-	ofBuffer geometryShaderBuffer = ofBufferFromFile( ofToDataPath( geometryShaderFilename ) );
-	
-	fileChangedTimes.clear();
-	fileChangedTimes.push_back( getLastModified( vertexShaderFile ) );
-	fileChangedTimes.push_back( getLastModified( fragmentShaderFile ) );
-	fileChangedTimes.push_back( getLastModified( geometryShaderFile ) );
-	
-	if ( vertexShaderBuffer.size() > 0 ) {
-		setupShaderFromSource(GL_VERTEX_SHADER, vertexShaderBuffer.getText() );
+	if ( vertexSrc.size() > 0 ) {
+		setupShaderFromSource( GL_VERTEX_SHADER, version_vert_header + defines_header + vertexSrc );
 	}
 
-	if ( fragmentShaderBuffer.size() > 0 ) {
-		setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShaderBuffer.getText());
+	if ( fragmentSrc.size() > 0 ) {
+		setupShaderFromSource( GL_FRAGMENT_SHADER, version_frag_header + defines_header + fragmentSrc );
 	}
 
 	#ifndef TARGET_OPENGLES
-	if ( geometryShaderBuffer.size() > 0 ) {
-		setupShaderFromSource(GL_GEOMETRY_SHADER_EXT, geometryShaderBuffer.getText());
+	if ( geometrySrc.size() > 0 ) {
+		setupShaderFromSource( GL_GEOMETRY_SHADER_EXT, version_geom_header + defines_header + geometrySrc );
 	}
 	#endif
 
@@ -73,78 +210,119 @@ bool ofxSmartShader::load(string _vertName, string _fragName, string _geomName) 
 	return linkProgram();
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
-void ofxSmartShader::_update(ofEventArgs &e) {
-	if ( loadShaderNextFrame ) {
+bool find_id(const std::string& program, const char* id) {
+    return std::strstr(program.c_str(), id) != 0;
+}
+
+void ofxSmartShader::_checkActiveUniforms(std::string &_source) {
+    if (!m_time)
+        m_time = find_id(_source, "u_time");
+    if (!m_date)
+        m_date = find_id(_source, "u_date");
+    if (!m_delta)
+        m_delta = find_id(_source, "u_delta");
+    if (!m_mouse)
+        m_mouse = find_id(_source, "u_mouse");
+    if (!m_resolution)
+        m_resolution = find_id(_source, "u_resolution");
+}
+
+void ofxSmartShader::begin() {
+    ofShader::begin();
+    
+    if (m_time)
+        setUniform1f("u_time", ofGetElapsedTimef());
+    
+    if (m_date)
+        setUniform4f("u_date", ofGetYear(), ofGetMonth(), ofGetDay(), ofGetSeconds());
+    
+    if (m_delta) {
+        double now = ofGetElapsedTimef();
+        setUniform1f("u_delta", now - m_lastFrame);
+        m_lastFrame = now;
+    }
+    
+    if (m_mouse)
+        setUniform2f("u_mouse", ofGetMouseX(), ofGetMouseY());
+    
+    if (m_resolution)
+        setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
+}
+
+void ofxSmartShader::_update (ofEventArgs &e) {
+	if ( m_loadShaderNextFrame ) {
 		reloadShaders();
-		loadShaderNextFrame = false;
+		m_loadShaderNextFrame = false;
 	}
 	
 	int currTime = ofGetElapsedTimeMillis();
 	
-	if (((currTime - lastTimeCheckMillis) > millisBetweenFileCheck) &&
-	   	!loadShaderNextFrame ) {
+	if (((currTime - m_lastTimeCheckMillis) > m_millisBetweenFileCheck) &&
+	   	!m_loadShaderNextFrame ) {
 		if ( filesChanged() ) {
-			loadShaderNextFrame = true;
+			m_loadShaderNextFrame = true;
 		}
 		
-		lastTimeCheckMillis = currTime;
+		m_lastTimeCheckMillis = currTime;
 	}
 }
 
-
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 bool ofxSmartShader::reloadShaders() {
-	return load( vertexShaderFilename,  fragmentShaderFilename, geometryShaderFilename );
+	return load( m_vertexShaderFilename,  m_fragmentShaderFilename, m_geometryShaderFilename );
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
+void ofxSmartShader::enableAutoVersionConversion() {
+    if (!m_bAutoVersionConversion) {
+        m_loadShaderNextFrame = true;
+    }
+    m_bAutoVersionConversion = true;
+}
+
+void ofxSmartShader::disableAutoVersionConversion() {
+    if (m_bAutoVersionConversion) {
+        m_loadShaderNextFrame = true;
+    }
+    m_bAutoVersionConversion = false;
+}
+
 void ofxSmartShader::enableWatchFiles() {
-	if (!bWatchingFiles) {
-		ofAddListener(ofEvents().update, this, &ofxSmartShader::_update );
-		bWatchingFiles = true;
+	if (!m_bWatchingFiles) {
+		ofAddListener( ofEvents().update, this, &ofxSmartShader::_update );
+		m_bWatchingFiles = true;
 	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 void ofxSmartShader::disableWatchFiles() {
-	if (bWatchingFiles) {
-		ofRemoveListener(ofEvents().update, this, &ofxSmartShader::_update );
-		bWatchingFiles = false;
+	if (m_bWatchingFiles) {
+		ofRemoveListener( ofEvents().update, this, &ofxSmartShader::_update );
+		m_bWatchingFiles = false;
 	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 bool ofxSmartShader::filesChanged() {
 	bool fileChanged = false;
 	
-	if ( vertexShaderFile.exists() ) {
-		std::time_t vertexShaderFileLastChangeTime = getLastModified( vertexShaderFile );
-		if( vertexShaderFileLastChangeTime != fileChangedTimes.at(0) )
-		{
-			fileChangedTimes.at(0) = vertexShaderFileLastChangeTime;
+	if ( m_vertexShaderFile.exists() ) {
+		std::time_t vertexShaderFileLastChangeTime = getLastModified( m_vertexShaderFile );
+		if ( vertexShaderFileLastChangeTime != m_fileChangedTimes.at(0) ) {
+			m_fileChangedTimes.at(0) = vertexShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
 	
-	if ( fragmentShaderFile.exists() ) {
-		std::time_t fragmentShaderFileLastChangeTime = getLastModified( fragmentShaderFile );
-		if ( fragmentShaderFileLastChangeTime != fileChangedTimes.at(1) ) {
-			fileChangedTimes.at(1) = fragmentShaderFileLastChangeTime;
+	if ( m_fragmentShaderFile.exists() ) {
+		std::time_t fragmentShaderFileLastChangeTime = getLastModified( m_fragmentShaderFile );
+		if ( fragmentShaderFileLastChangeTime != m_fileChangedTimes.at(1) ) {
+			m_fileChangedTimes.at(1) = fragmentShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
 	
 	
-	if ( geometryShaderFile.exists() ) {
-		std::time_t geometryShaderFileLastChangeTime = getLastModified( geometryShaderFile );
-		if ( geometryShaderFileLastChangeTime != fileChangedTimes.at(2) ) {
-			fileChangedTimes.at(2) = geometryShaderFileLastChangeTime;
+	if ( m_geometryShaderFile.exists() ) {
+		std::time_t geometryShaderFileLastChangeTime = getLastModified( m_geometryShaderFile );
+		if ( geometryShaderFileLastChangeTime != m_fileChangedTimes.at(2) ) {
+			m_fileChangedTimes.at(2) = geometryShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
@@ -152,8 +330,6 @@ bool ofxSmartShader::filesChanged() {
 	return fileChanged;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 std::time_t ofxSmartShader::getLastModified( ofFile& _file ) {
 	if ( _file.exists() ) {
         return std::filesystem::last_write_time(_file.path());
@@ -163,26 +339,21 @@ std::time_t ofxSmartShader::getLastModified( ofFile& _file ) {
 	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 void ofxSmartShader::setMillisBetweenFileCheck( int _millis ) {
-	millisBetweenFileCheck = _millis;
+	m_millisBetweenFileCheck = _millis;
 }
 
-//--------------------------------------------------------------
 void ofxSmartShader::setGeometryInputType( GLenum _type ) {
     ofShader::setGeometryInputType(_type);
-    geometryInputType = _type;
+    m_geometryInputType = _type;
 }
 
-//--------------------------------------------------------------
 void ofxSmartShader::setGeometryOutputType( GLenum _type ) {
     ofShader::setGeometryOutputType(_type);
-    geometryOutputType = _type;
+    m_geometryOutputType = _type;
 }
 
-//--------------------------------------------------------------
 void ofxSmartShader::setGeometryOutputCount( int _count ) {
     ofShader::setGeometryOutputCount(_count);
-    geometryOutputCount = _count;
+    m_geometryOutputCount = _count;
 }
--- src/ofxSmartShader.h
+++ src/ofxSmartShader.h
@@ -3,51 +3,75 @@
 #include "ofMain.h"
 
 class ofxSmartShader : public ofShader {
- public:
+public:
 	ofxSmartShader();
-	~ofxSmartShader();
+	virtual ~ofxSmartShader();
 	
 	// override the initialisation functions
-	bool load(string shaderName );
-	bool load(string vertName, string fragName, string geomName);
+	bool    load(string _shaderName);
+	bool    load(string _vertName, string _fragName, string _geomName);
 	
-	bool reloadShaders();
+	bool    reloadShaders();
 	
-	void enableWatchFiles();
-	void disableWatchFiles();
-		
-	void setMillisBetweenFileCheck( int _millis );
-	
-	void _update(ofEventArgs &e);
+	void    enableWatchFiles();
+	void    disableWatchFiles();
     
-    void setGeometryInputType(GLenum type);
-    void setGeometryOutputType(GLenum type);
-    void setGeometryOutputCount(int count);
+    void    enableAutoVersionConversion();
+    void    disableAutoVersionConversion();
+		
+	void    setMillisBetweenFileCheck(int _millis);
+    
+    void    setGeometryInputType(GLenum _type);
+    void    setGeometryOutputType(GLenum _type);
+    void    setGeometryOutputCount(int _count);
+    void    setConversionVersion(int _version);
+    
+    void    addIncludeFolder(std::string &_folder);
+    
+    void    addDefineKeyword(std::string &_define);
+    void    delDefineKeyword(std::string &_define);
+    
+    void    begin();
+    
+protected:
+    void    _update(ofEventArgs &e);
+    void    _checkActiveUniforms(std::string &_source);
 	
 private:
-	
-	bool bWatchingFiles;
-	bool filesChanged();
-	
-	bool loadShaderNextFrame;
-	
-	std::time_t getLastModified( ofFile& _file );
-	
-	int lastTimeCheckMillis;
-	int millisBetweenFileCheck;
-	
-	string vertexShaderFilename;
-	string fragmentShaderFilename;
-	string geometryShaderFilename;
-	
-	ofFile vertexShaderFile;
-	ofFile fragmentShaderFile;
-	ofFile geometryShaderFile;
-	
-	vector< std::time_t > fileChangedTimes;
+    std::time_t getLastModified(ofFile& _file);
+    bool        filesChanged();
     
-    GLenum geometryInputType, geometryOutputType;
-    int geometryOutputCount;
+    vector< std::string >   m_defines;
+    vector< std::string >   m_includeFolders;
+    vector< std::time_t >   m_fileChangedTimes;
+    
+    ofFile                  m_vertexShaderFile;
+    ofFile                  m_fragmentShaderFile;
+    ofFile                  m_geometryShaderFile;
+    
+    string                  m_vertexShaderFilename;
+    string                  m_fragmentShaderFilename;
+    string                  m_geometryShaderFilename;
+
+    double                  m_lastFrame;
+    
+    GLenum                  m_geometryInputType;
+    GLenum                  m_geometryOutputType;
+    int                     m_geometryOutputCount;
+    
+    int                     m_lastTimeCheckMillis;
+    int                     m_millisBetweenFileCheck;
+    int                     m_version;
+    
+    bool                    m_bAutoVersionConversion;
+    bool                    m_bWatchingFiles;
+    bool                    m_loadShaderNextFrame;
+    
+    bool                    m_time;
+    bool                    m_date;
+    bool                    m_delta;
+    bool                    m_mouse;
+    bool                    m_resolution;
 };
 
 
