eac28e9554e0823c44db86ddffcd76d48b98a0c8
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-04-28 13:17:29 -0700

    adding M_PI_2

--- src/ofxTextureCube.cpp
+++ src/ofxTextureCube.cpp
@@ -5,6 +5,10 @@
 #define M_PI 3.14159265358979323846
 #endif 
 
+#ifndef M_PI_2 
+#define M_PI_2 1.57079632679489661923
+#endif
+
 #ifndef M_2_SQRTPI
 #define M_2_SQRTPI 1.12837916709551257390
 #endif
